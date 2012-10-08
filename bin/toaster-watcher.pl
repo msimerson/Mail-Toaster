68852ee2eb6f8d9446c8d4fcce1b86cb3bf18b5d
Matt Simerson <matt@tnpi.net>
2012-10-08 15:15:10 -0700

    init a $setup object so $setup-> method will work

--- bin/toaster-watcher.pl
+++ bin/toaster-watcher.pl
@@ -21,6 +21,7 @@ my $toaster = Mail::Toaster->new( debug => $opt_v );
 my $util   = $toaster->get_util;
 my $conf   = $toaster->get_config();
 my $qmail  = Mail::Toaster::Qmail->new( 'log' => $toaster );
+my $setup  = Mail::Toaster::Setup->new( 'log' => $toaster, conf => $conf );
 my $debug  = $conf->{'toaster_debug'} || $opt_v || 0;
 
 my $pidfile = "/var/run/toaster-watcher.pid";
